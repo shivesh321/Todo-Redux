380a46a9e6a08371870547ef854e1e68d7f8f558
Shivesh kr jha <shiveshkrjha2015@gmail.com>
2023-11-29 15:12:02 +0530

    Todo App built with Redux Toolkit!

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
 
 function App() {
   return (
     <>
-      <h1>TO DO</h1>
+      <h1 className="text-white text-lg font-bold ">Write Your TODOS</h1>
+      <AddTodo />
+      <Todos />
     </>
   );
 }
--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = {
-  todos: [{ id: 1, text: "hello world" }],
+  todos: [],
 };
 
 export const todoSlice = createSlice({
@@ -16,11 +16,18 @@ export const todoSlice = createSlice({
       state.todos.push(todo);
     },
     removeTodo: (state, action) => {
-      state.todos.filter((todo) => todo.id !== action.payload);
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+    updateTodo: (state, action) => {
+      const { id, newText } = action.payload;
+      const todoToUpdate = state.todos.find((todo) => todo.id === id);
+      if (todoToUpdate) {
+        todoToUpdate.text = newText;
+      }
     },
   },
 });
 
-export const { addTodo, removeTodo } = todoSlice.actions;
+export const { addTodo, removeTodo, updateTodo } = todoSlice.actions;
 
 export default todoSlice.reducer;
